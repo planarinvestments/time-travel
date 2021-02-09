a6516aa79a563972ba503ff8c565382e8485678b
pragman <mahadevan.k@gmail.com>
2021-02-09 11:34:14 +0530

    fixed enum specs

--- spec/lib/enum_time_travel_model_spec.rb
+++ spec/lib/enum_time_travel_model_spec.rb
@@ -5,11 +5,11 @@ describe TimeTravel do
   let(:klass) do
     Class.new(ActiveRecord::Base) do
       self.table_name = 'performance_data'
-      include TimeTravel
+      include TimeTravel::TimelineHelper
       default_scope { order(effective_from: :desc) }
 
       enum status: { recorded: 0, stale: 1, fresh: 2 }
-      def self.time_travel_identifiers
+      def self.timeline_fields
         [:wrapper_id, :reporting_currency]
       end
 
@@ -24,10 +24,13 @@ describe TimeTravel do
   let(:amount) { 150 }
   let(:effective_from) { DateTime.parse('01-01-2020') }
 
+  let(:timeline) {
+    klass.timeline(wrapper_id: wrapper_id, reporting_currency: reporting_currency)
+  }
+
   describe 'create' do
     it "creates new record with timestamps set" do
-      new_obj = klass.create(amount: amount, wrapper_id: wrapper_id, status: 'stale',
-        reporting_currency: reporting_currency, effective_from: effective_from)
+      new_obj = timeline.create(amount: amount, status: 'stale', effective_from: effective_from)
       expect(new_obj).to be_persisted
       expect(new_obj.wrapper_id).to eql(wrapper_id)
       expect(new_obj).to be_stale
@@ -42,9 +45,9 @@ describe TimeTravel do
   describe 'update' do
     it "updates record with enum values" do
       new_effective_from = effective_from + 2.days
-      new_obj = klass.create(amount: amount, wrapper_id: wrapper_id, status: 'stale',
-        reporting_currency: reporting_currency, amount: amount, effective_from: effective_from)
-      new_obj.update!(status: 'fresh', amount: 200, effective_from: new_effective_from)
+      timeline.create(amount: amount, status: 'stale', effective_from: effective_from)
+      timeline.update(status: 'fresh', amount: 200, effective_from: new_effective_from)
+      new_obj=timeline.effective_at(new_effective_from)
 
       expect(new_obj).to be_persisted
       expect(new_obj.wrapper_id).to eql(wrapper_id)
@@ -55,7 +58,7 @@ describe TimeTravel do
       expect(new_obj).to be_effective_now
       expect(new_obj).to be_valid_now
 
-      old_obj = klass.as_of(effective_from, wrapper_id, reporting_currency)
+      old_obj = timeline.effective_at(effective_from)
       expect(old_obj.effective_from.to_datetime).to eql(effective_from)
       expect(old_obj.effective_till.to_datetime).to eql(new_effective_from)
       expect(old_obj).to be_stale
